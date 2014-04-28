561dce87ec82d9ac78c11b3d2a422add314eed7f
FrankFan <fanyong@gmail.com>
2014-04-29 00:00:35 +0800

    first commit code

---
+++ FreeWiFi/Main.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace FreeWiFi
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SSID(wifi name)热点名称:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(182, 20);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(179, 21);
+            this.txtName.TabIndex = 1;
+            this.txtName.Text = "myWiFi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password/密 码:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(182, 63);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(179, 21);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnInit
+            // 
+            this.btnInit.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnInit.Location = new System.Drawing.Point(29, 98);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(95, 51);
+            this.btnInit.TabIndex = 4;
+            this.btnInit.Text = "1.初始化";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStart.Location = new System.Drawing.Point(149, 98);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 51);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "2.启动热点";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnEnd.Location = new System.Drawing.Point(268, 98);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(93, 51);
+            this.btnEnd.TabIndex = 4;
+            this.btnEnd.Text = "3.结束";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(29, 168);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(332, 152);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 337);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FreeWiFi 自由WiFi  by fanyong@gmail.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ FreeWiFi/Main.cs
@@ -0,0 +1,117 @@
+﻿/*
+ *  Description：WiFI助手 V0.0.1
+ *  Author: FrankFan
+ *  Email:fanyong@gmail.com
+ */
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace FreeWiFi
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            string ssid = txtName.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            bool isValid = isValidInput(ssid, password);
+
+            if (!isValid)
+                return;
+
+            Initial(ssid, password);
+
+
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartWiFi();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.txtOutput.Text = string.Empty;
+
+            string cmd = string.Format(@"netsh wlan stop hostednetwork");
+            ExecuteCmd(cmd);
+        }
+
+        private bool isValidInput(string ssid, string password)
+        {
+            if (string.IsNullOrEmpty(ssid))
+            {
+                MessageBox.Show("SSID不能为空.");
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                MessageBox.Show("密码至少需要8位.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Initial(string ssid, string password)
+        {
+            this.txtOutput.Text = string.Empty;
+
+            string cmd = string.Format(@"netsh wlan set hostednetwork mode=allow ssid={0} key=""{1}""", ssid, password);
+
+        }
+
+        private void ExecuteCmd(string cmd)
+        {
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
+            cmdStartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            cmdStartInfo.RedirectStandardOutput = true;
+            cmdStartInfo.RedirectStandardError = true;
+            cmdStartInfo.RedirectStandardInput = true;
+            cmdStartInfo.UseShellExecute = false;
+            cmdStartInfo.CreateNoWindow = true;
+
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo = cmdStartInfo;
+            cmdProcess.ErrorDataReceived += cmdProcess_ErrorDataReceived;
+            cmdProcess.OutputDataReceived += cmdProcess_OutputDataReceived;
+            cmdProcess.EnableRaisingEvents = true;
+            cmdProcess.Start();
+            cmdProcess.BeginOutputReadLine();
+            cmdProcess.BeginErrorReadLine();
+
+            cmdProcess.StandardInput.WriteLine(cmd);     //Execute ping bing.com
+            cmdProcess.StandardInput.WriteLine("exit");                  //Execute exit.
+
+            cmdProcess.WaitForExit();
+        }
+
+        void cmdProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.txtOutput.Text += (e.Data + "\r\n");
+        }
+
+        void cmdProcess_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.txtOutput.Text += (e.Data + "\r\n");
+        }
+
+        private void StartWiFi()
+        {
+            this.txtOutput.Text = string.Empty;
+
+            string cmd = string.Format(@"netsh wlan start hostednetwork");
+            ExecuteCmd(cmd);
+        }
+
+    }
+}
